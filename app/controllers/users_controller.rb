05ce84b4086d2fb0c0518036f382abf6e6de4f4c
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-11-17 16:19:30 -0700

    Add create action and show redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,21 @@ class UsersController < ApplicationController
 	def new
     @user = User.new
 	end
-end
 
+  def create
+    user = User.create(user_params)
+    if user
+      flash[:success] = "Account successfully created!"
+      redirect_to(profile_path(user))
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :street_address, :city, :state, :zip_code, :email, :password, :password_confirmation)
+  end
+end
