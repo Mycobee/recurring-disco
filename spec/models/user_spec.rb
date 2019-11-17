6cdbc683873f4d1e9843cf2158a0a7ae44cbaa0a
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-11-17 16:39:11 -0700

    Add model validations and specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do 
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :password }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :street_address }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_numericality_of :zip_code }
+    it { should validate_presence_of :zip_code }
+    it { should validate_presence_of :recurring }
+    it { should validate_inclusion_of(:recurring).in_array([true, false]) }
+  end
 end
