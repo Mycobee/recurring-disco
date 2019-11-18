4abe9ca163f3ab0f12146bd625c8b9bfe6f39698
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-11-17 21:25:23 -0700

    Add article resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_17_215619) do
+ActiveRecord::Schema.define(version: 2019_11_18_041549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
