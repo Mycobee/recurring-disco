df17aaef54892bb421be6cdd861bd19b7ce69d51
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-11-18 11:44:35 -0700

    Add completed happy path recurring user comment spec

--- spec/features/articles/comments/recurring_user_comments_spec.rb
+++ spec/features/articles/comments/recurring_user_comments_spec.rb
@@ -5,17 +5,20 @@ describe "As a user on an article show page" do
 		before :each do
 	    @non_recurring_user = create(:non_recurring_user)	
 	    @user = create(:user)	
+      @article = create(:article)
 		end
 		
 		it "Posts the comment and displays it at the top of the list" do
+      login_as(@user)
 	    visit article_path(@article)
-      comment_text = "Comments can be long or short, witty or crude, polite or mean.  But commentary is a necessary part of human discourse, therefore I hope to facilitate it with this text"
-      fill_in "#comment-box", with: comment_text
+      comment_text = "Comments can be long or short, witty or crude, polite or mean. But commentary is a necessary part of human discourse, therefore I hope to facilitate it with this text"
+      fill_in "body", with: comment_text
       click_button "Add Comment"
-
       expect(current_path).to eq(article_path(@article))
 
-      expect(page).to have_content(comment_text)
+      within ".comment-body" do
+        expect(page).to have_content(comment_text)
+      end
 		end
 	end
 end
