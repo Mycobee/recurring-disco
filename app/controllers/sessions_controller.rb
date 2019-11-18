517b892ec0762b6bb021cc7675a09e3b6a32397f
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-11-17 20:00:53 -0700

    Add sessions create functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,30 @@
 class SessionsController < ApplicationController
-	def new
+  def new
+    if current_user
+      flash[:warning] = "You are already logged in."
+      redirect_user(current_user)
+    end
+  end
+  
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "You are now logged in!"
+      redirect_user(user)
+    else
+      flash.now[:danger] = "Sorry, that email and password don't match."
+      render :new
+    end
+  end
 
-	end
+  private
+
+  def redirect_user(user)
+    if user.default?
+      redirect_to profile_path(current_user)
+    elsif user.admin?
+      redirect_to root_path
+    end
+  end
 end
-
