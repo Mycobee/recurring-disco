3786e04c74084d794008d4b36a1f57dc166200a3
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-11-18 11:48:29 -0700

    Clean up formatting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
-	def index
+  def index
     @articles = Article.all
-	end
+  end
 
   def show
     @article = Article.find(params[:id])
     redirect_to recurly_path if non_recurring
   end
 end
-
