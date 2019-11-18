a0170ec77b81a9bfcee790be343653d845865e6e
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-11-17 18:10:38 -0700

    Add sequences to users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,17 @@
 FactoryBot.define do
-  factory :user do
-    name { "MyString" }
-    email { "MyString" }
-    password_digest { "MyString" }
-    street_address { "MyString" }
-    city { "MyString" }
-    state { "MyString" }
-    zip_code { 1 }
+  factory :user, class: User do
+    sequence(:email) { |n| "user_#{n}@gmail.com" }
+    password { "password" }
+    sequence(:name) { |n| "User Name #{n}" }
+    sequence(:street_address) { |n| "Address #{n}" }
+    sequence(:city) { |n| "City #{n}" }
+    sequence(:state) { |n| "State #{n}" }
+    sequence(:zip_code) { |n| n }
+    recurring { true }
+  end
+  factory :inactive_user, parent: :user do
+    sequence(:name) { |n| "Inactive User Name #{n}" }
+    sequence(:email) { |n| "inactive_user_#{n}@gmail.com" }
     recurring { false }
   end
 end
