9ada1344df4b6c233abd1b683ca17d3bcb2601e4
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-11-17 20:48:48 -0700

    Add navbar spec for logged in user

---
+++ spec/features/navigation/user_navigation_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "As a logged in user to the site" do
+  describe "I see a navigation bar" do
+    it "contains links to the home, logout, profile, and articles paths" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit root_path
+
+      within(".navbar") do
+        expect(current_path).to eq("/")  
+        expect(page).to have_link("Log out")
+
+        click_link("Profile")
+        expect(current_path).to eq(profile_path(user))
+
+        click_link("Articles")
+        expect(current_path).to eq("/articles")
+
+        click_link("Welcome")
+        expect(current_path).to eq(root_path)
+      end
+    end
+  end
+end
