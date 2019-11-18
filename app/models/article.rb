6f821ed861497c17d079fac67c3d4fa4973a33ad
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-11-18 15:45:57 -0700

    Add order_comments method and clean up feature test

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,10 @@ class Article < ApplicationRecord
   validates_presence_of :title, :body
 
   def preview
-    body.split(" ")[0..10].join(' ')
+    body.split(" ")[0..10].join(' ') + "..."
+  end
+
+  def order_comments
+    Article.where(id: self.id).includes(:comments).order('comments.created_at DESC').first
   end
 end
