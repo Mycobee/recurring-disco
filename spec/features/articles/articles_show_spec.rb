7a8311d0ec05b50351f9d513eb80b66b4a5dcec3
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-11-17 22:14:59 -0700

    Add sad path testing for non recurring user

--- spec/features/articles/articles_show_spec.rb
+++ spec/features/articles/articles_show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "As a reccuring user on the articles index page" do
+describe "As a user on the articles index page" do
 	describe "When I click an article title" do
 		before :each do
       @user_1 = create(:user)
@@ -9,18 +9,31 @@ describe "As a reccuring user on the articles index page" do
 			@article_2 = create(:article)	
 		end
 		
-		it "Renders the article show page, with the title and full body" do
+		it "Renders the article show page, with the title and full body for a recurring user" do
       login_as(@user_1)
 	    visit articles_path
 
       within "#article-#{@article_1.id}" do
         click_link(@article_1.title)
-
-        expect(current_path).to eq(article_path(@article_1))
-        expect(page).to have_content(@article_1.title)
-        expect(page).to have_content(@article_1.body)
       end
+
+      expect(current_path).to eq(article_path(@article_1))
+      expect(page).to have_content(@article_1.title)
+      expect(page).to have_content(@article_1.body)
 		end
+
+    it "Renders a Recurly sign up path for a non recurring user" do
+      login_as(@user_2)
+	    visit articles_path
+
+      within "#article-#{@article_1.id}" do
+        click_link(@article_1.title)
+      end
+
+      expect(current_path).to eq(new_recurly_path)
+      expect(page).to_not have_content(@article_1.title)
+      expect(page).to_not have_content(@article_1.body)
+    end 
 	end
 end
 
