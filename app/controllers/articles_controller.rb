813a6e68acd66448a865a805aa8dc38741b4a53b
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-11-18 15:46:17 -0700

    Add comments ivar for view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,8 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:id]).order_comments
+    @comments = @article.comments
     redirect_to recurly_path if non_recurring
   end
 end
