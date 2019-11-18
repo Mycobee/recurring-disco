747fd2a90bf9d87ec10ab50a64e573300acf4b47
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-11-18 09:10:17 -0700

    Add comment relationship and spec to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :comments
   validates :email, presence: true, uniqueness: true
   validates :zip_code, presence: true, numericality: true
   validates_presence_of :password, :name, :street_address, :city, :state
