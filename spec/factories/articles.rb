248e69358f3966e136076faa91d3bfaad1f88fc6
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-11-17 21:46:56 -0700

    Add sequences for articles factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    body { "MyString" }
+    sequence(:title) { |n| "Title #{n}" }
+    sequence(:body) { |n| "Lorem ipsum dolor sit amet, rebum putant ex has, et duo rebum saperet incorrupte. At tale dolores luptatum eum, id his libris copiosae philosophia, sumo putant reprimique id sit. Ut duo ferri oporteat concludaturque, id sumo clita laoreet nam. Sed labores scripserit in. #{n}" }
   end
 end
