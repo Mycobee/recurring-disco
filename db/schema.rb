9c905f04f234b3d0e6f3d750e0be37dedfa76216
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-11-17 21:27:12 -0700

    Change body from string to text field

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2019_11_18_041549) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
